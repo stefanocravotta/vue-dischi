75ea642e6a49fae85c4630cd47a427425e04e85f
stefanocravotta <you@example.com>
2022-05-17 16:33:52 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
 
+    <HeaderComponent />
+
+    <MainComponent />
+
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    
-  },
+    HeaderComponent,
+    MainComponent
+},
   data(){
     return{
       baseUrl: "https://flynn.boolean.careers/exercises/api/array/music",
@@ -33,5 +40,6 @@ export default {
 </script>
 
 <style lang="scss">
-
+@import "./assets/style/global";
+@import "./assets/style/vars";
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="d-flex justify-content-between">
+    <img src="https://cdn.iconscout.com/icon/free/png-256/spotify-14-437140.png" alt="">
+    <select class="form-select " aria-label="Default select example">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+    </select>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/vars";
+header{
+    padding: 5px 10px;
+    background-color: lighten($primary-color, 20%);
+    img{
+        max-width: 40px;
+    }
+    select{
+        max-width: 100px;
+    }
+}
+</style>
